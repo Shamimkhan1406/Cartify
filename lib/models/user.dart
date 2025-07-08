b848f93744d26451a4387fc707f56e2baa76899f
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-08 20:56:01 +0530

    created user model-defined fields

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+
+class user {
+  final String id;
+  final String fullName;
+  final String email;
+  final String state;
+  final String city;
+  final String locality;
+  final String password;
+
+  user({required this.id, required this.fullName, required this.email, required this.state, required this.city, required this.locality, required this.password});
+}
