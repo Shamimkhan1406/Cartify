32b3f80a5bb1d5f9df2992af7fcebb8d27df4e30
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-08 21:32:14 +0530

    deserialization: convert json string to user object

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 class User {
@@ -10,9 +9,17 @@ class User {
   final String locality;
   final String password;
 
-  User({required this.id, required this.fullName, required this.email, required this.state, required this.city, required this.locality, required this.password});
+  User({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.state,
+    required this.city,
+    required this.locality,
+    required this.password,
+  });
   // serialization: convert user object to a map
-  Map<String, dynamic>toMap(){
+  Map<String, dynamic> toMap() {
     return <String, dynamic>{
       "id": id,
       "fulName": fullName,
@@ -23,6 +30,22 @@ class User {
       "password": password,
     };
   }
+
   // convert map to json string
-  String toJson()=> json.encode(toMap());
+  String toJson() => json.encode(toMap());
+  // deserialization: convert json string to user object
+  // so it can be easily manipulated and use within the app
+  // the factory constructor takes map (usually comes from a json string)
+  // and returns a user object
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map["_id"] as String? ?? "",
+      fullName: map["fullName"] as String? ?? "",
+      email: map["email"] as String? ?? "",
+      state: map["state"] as String? ?? "",
+      city: map["city"] as String? ?? "",
+      locality: map["locality"] as String? ?? "",
+      password: map["password"] as String? ?? "",
+    );
+  }
 }
