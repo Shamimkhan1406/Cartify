5b9aa23ca80a91582484b46abf2a7d62ad536d8b
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-08 21:05:13 +0530

    serialized user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -9,4 +9,16 @@ class user {
   final String password;
 
   user({required this.id, required this.fullName, required this.email, required this.state, required this.city, required this.locality, required this.password});
+  // serialization: convert user object to a map
+  Map<String, dynamic>toMap(){
+    return <String, dynamic>{
+      "id": id,
+      "fulName": fullName,
+      "email": email,
+      "state": state,
+      "city": city,
+      "locality": locality,
+      "password": password,
+    };
+  }
 }
