e50c63a54d7f5996445e6c117412fb798537c9d9
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-19 23:04:23 +0530

    left side display categories

--- lib/views/screens/nav_screens/category_screen.dart
+++ lib/views/screens/nav_screens/category_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cartify/controllers/category_controller.dart';
+import 'package:cartify/models/category.dart';
 import 'package:cartify/views/screens/nav_screens/widgets/banner_widget.dart';
 import 'package:cartify/views/screens/nav_screens/widgets/header_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -10,13 +13,64 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  // a future that will hold the list of categories once loaded from the api
+  late Future<List<Category>> futureCategories;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureCategories = CategoryController().loadCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        HeaderWidget(),
-        BannerWidget(),
-      ],
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).size.height * 0.15,
+        ),
+        child: HeaderWidget(),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.grey.shade50,
+              child: FutureBuilder(
+                future: futureCategories,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No categories available'));
+                  } else {
+                    final categories = snapshot.data!;
+
+                    return ListView.builder(
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        final category = categories[index];
+                        return ListTile(
+                          title: Text(
+                            category.name,
+                            style: GoogleFonts.quicksand(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
