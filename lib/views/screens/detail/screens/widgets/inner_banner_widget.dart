993a91ef37aa367ff60c3ea28a8fa7dca894b4e3
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-20 23:47:54 +0530

    displayed category banners

---
+++ lib/views/screens/detail/screens/widgets/inner_banner_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class InnerBannerWidget extends StatelessWidget {
+  final String image;
+
+  const InnerBannerWidget({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 170,
+        width: MediaQuery.of(context).size.width,
+        // decoration: BoxDecoration(
+        //   borderRadius: BorderRadius.circular(4),
+        //   color: Colors.red, //Color(0xFFF7F7F7)
+        // ),
+        // clipBehavior: Clip.hardEdge,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(4),
+          child: FittedBox(child: Image.network(image, fit: BoxFit.cover)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/detail/screens/widgets/inner_header_widget.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class InnerHeaderWidget extends StatelessWidget {
+  const InnerHeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.15,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: [
+          Image.asset(
+            "assets/icons/searchBanner.jpeg",
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Positioned(left: 10, top: 76, child: IconButton(onPressed: (){
+            Navigator.of(context).pop();
+          }, icon: Icon(Icons.arrow_back_ios, color: Colors.white),),),
+          Positioned(
+            left: 50,
+            top: 70,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.height * 0.2,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: TextStyle(fontSize: 15, color: Colors.white),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 16,
+                  ),
+                  prefixIcon: Image.asset(
+                    "assets/icons/search.png",
+                    width: 28,
+                    height: 28,
+                  ),
+                  suffixIcon: Image.asset(
+                    'assets/icons/cam.png',
+                    width: 20,
+                    height: 20,
+                  ),
+                  filled: true,
+                  fillColor: Color(0x523A3A3A),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 311,
+            top: 82,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                onTap: () {},
+                overlayColor: WidgetStateProperty.all(Color(0x523A3A3A)),
+                child: Ink(
+                  width: 31,
+                  height: 31,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/icons/bell.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 364,
+            top: 82,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                onTap: () {},
+                overlayColor: WidgetStateProperty.all(Color(0x523A3A3A)),
+                child: Ink(
+                  width: 31,
+                  height: 31,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/icons/message.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
