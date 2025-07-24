33a21e9063089f5ca28a8415f57b0706a37f4d12
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-24 19:13:58 +0530

    manage the user session

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,12 +1,17 @@
 import 'package:cartify/global_variables.dart';
 import 'package:cartify/models/user.dart';
+import 'package:cartify/provider/user_provider.dart';
 import 'package:cartify/services/manage_http_response.dart';
 import 'package:cartify/views/screens/authentication_screen/login_screen.dart';
 import 'package:cartify/views/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
+final providerContainer = ProviderContainer();
 
 class AuthController {
   // sign up user
@@ -63,7 +68,22 @@ class AuthController {
           }
       );
       // handle the response by calling the manageHttpResponse function
-      manageHttpResponse(response: response, context: context, onSuccess: (){
+      manageHttpResponse(response: response, context: context, onSuccess: () async {
+        // access shared prefferences to save the token and user data storage
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        // Extract the authentication token from res body
+        String token = jsonDecode(response.body)['token'];
+        // strore the auth token in shared preferences
+        await preferences.setString("auth_token", token);
+        // encode the user data received from the backend as json
+        final userJson = jsonEncode(jsonDecode(response.body)['user']);
+        // update the app state with the user data using reverpod
+        providerContainer.read(userProvider.notifier).setUser(userJson);
+        // store the user data in shared preferences for future use
+        await preferences.setString("user", userJson);
+        
+
+
         // show a snackbar with the message
         showSnackBar(context, "logged in successfully");
         // navigate to the main screen
---
+++ lib/provider/user_provider.dart
@@ -0,0 +1,30 @@
+import 'package:cartify/models/user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserProvider extends StateNotifier<User?> {
+  //constructor initialize with default User Object
+  // perpose : Manege the state of the user allowing updates
+  UserProvider()
+    : super(
+        User(
+          id: '',
+          fullName: '',
+          email: '',
+          state: '',
+          city: '',
+          locality: '',
+          password: '',
+          token: '',
+        ),
+      );
+  // getter methode to extract the value of an object
+  User? get uset => state;
+  // method to set user state fromJson
+  // perpose: update the state of the user object based on the json data
+  void setUser(String userjson){
+    state = User.fromJson(userjson);
+  }
+  
+}
+// make the data accecsible within the app
+  final userProvider = StateNotifierProvider<UserProvider, User?> ((ref)=> UserProvider());
