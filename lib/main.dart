9c261a857c6cbaa5f7a420c90f9067ae894edb9b
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-24 22:57:57 +0530

    maintainting user state

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
 //import 'package:cartify/views/screens/authentication_screen/login_screen.dart';
+import 'package:cartify/provider/user_provider.dart';
+import 'package:cartify/views/screens/authentication_screen/login_screen.dart';
 import 'package:cartify/views/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future <void> main() async {
+  // run the flutter app wrapped in a providerScope for managing state
   await dotenv.load();
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+// root widget to the app,  a consumerWidget to consume state changes
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
+  // methode to check the token and set the user data if available
+  Future <void> _checkTokenAndSetUser(WidgetRef ref)async{
+    // obtain a instance of shared preffences for local data storage
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    // retrive the user auth token and user data stored locally
+    String? token = preferences.getString('auth_token');
+    String? userJson = preferences.getString('user');
+    // if both token and user data available, update the user state
+    if(token != null && userJson != null){
+      ref.read(userProvider.notifier).setUser(userJson);
+    }
+    
+  }
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
@@ -35,7 +54,16 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MainScreen(),
+      home: FutureBuilder(
+        future: _checkTokenAndSetUser(ref),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(child: CircularProgressIndicator());
+          }
+          final user = ref.watch(userProvider);
+          return user == null ? LoginScreen() : MainScreen();
+        },
+      ),
     );
   }
 }
