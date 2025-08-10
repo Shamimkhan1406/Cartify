5ef7d666bfac21b67b7100f38018d076cce9c1e2
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-08-10 20:46:39 +0530

    fetch products by category

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -59,4 +59,30 @@ class ProductController {
       
     }
   }
+  // fetch product by category
+  Future<List<Product>> loadProductByCategory(String category) async {
+    try {
+      http.Response response = await http.get(Uri.parse('$uri/api/products-by-category/$category'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      // check the http response status code
+      if (response.statusCode == 200) {
+        // decode the json response body into list of dynamic objects type
+        final List<dynamic> data = jsonDecode(response.body) as List<dynamic>;
+
+        // map the list of dynamic objects to a list of Product objects
+        List<Product> products = data.map((product)=> Product.fromMap(product as Map<String, dynamic>)).toList();
+        return products;
+      } else {
+        // if the response status code is not 200, throw an error
+        throw Exception('Failed to load recommended products');
+      }
+    } catch (e) {
+      // Handle error
+      throw Exception('Error loading products by category: $e');
+      
+    }
+  }
 }
