ee09cf8c3ae0b796309002fa83985f01455b6cb2
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-08-11 17:06:12 +0530

    created product detail screen

---
+++ lib/views/screens/detail/screens/product_detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:cartify/models/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatefulWidget {
+  final Product product;
+
+  const ProductDetailScreen({super.key, required this.product});
+
+  @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:Text(widget.product.productName)),
+    );
+  }
+}
--- lib/views/screens/nav_screens/widgets/product_item_widget.dart
+++ lib/views/screens/nav_screens/widgets/product_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:cartify/models/product.dart';
+import 'package:cartify/views/screens/detail/screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,60 +10,65 @@ class ProductItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 170,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(24),
-              color: Colors.blueGrey,
-            ),
-            child: Stack(
-              children: [
-                Image.network(
-                  product.images[0],
-                  height: 170,
-                  width: 170,
-                  fit: BoxFit.cover,
-                ),
-                Positioned(
-                  right: 4,
-                  top: 15,
-                  child: Image.asset(
-                    'assets/icons/love.png',
-                    width: 26,
-                    height: 26,
-                  ),
-                ),
-                Positioned(
-                  bottom: 4,
-                  right: 4,
-                  child: Image.asset(
-                    'assets/icons/cart.png',
-                    width: 26,
-                    height: 26,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 8),
-          Text(
-            product.productName,
-            overflow: TextOverflow.ellipsis,
-            style: GoogleFonts.roboto(fontSize: 14,fontWeight: FontWeight.w500),
-          ),
-          SizedBox(height: 4),
-          Text(product.category,
-              style: GoogleFonts.quicksand(
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>ProductDetailScreen(product: product)));
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 170,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
                 color: Colors.blueGrey,
-              ))
-        ],
+              ),
+              child: Stack(
+                children: [
+                  Image.network(
+                    product.images[0],
+                    height: 170,
+                    width: 170,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    right: 4,
+                    top: 15,
+                    child: Image.asset(
+                      'assets/icons/love.png',
+                      width: 26,
+                      height: 26,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 4,
+                    right: 4,
+                    child: Image.asset(
+                      'assets/icons/cart.png',
+                      width: 26,
+                      height: 26,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              product.productName,
+              overflow: TextOverflow.ellipsis,
+              style: GoogleFonts.roboto(fontSize: 14,fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 4),
+            Text(product.category,
+                style: GoogleFonts.quicksand(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.blueGrey,
+                ))
+          ],
+        ),
       ),
     );
   }
