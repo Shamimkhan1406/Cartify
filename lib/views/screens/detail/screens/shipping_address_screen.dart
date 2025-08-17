a4d63a8075670e27b955edd037e9e641bf966551
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-08-18 00:37:25 +0530

    shipping address form validation

--- lib/views/screens/detail/screens/shipping_address_screen.dart
+++ lib/views/screens/detail/screens/shipping_address_screen.dart
@@ -9,6 +9,7 @@ class ShippingAddressScreen extends StatefulWidget {
 }
 
 class _ShippingAddressScreenState extends State<ShippingAddressScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,63 +30,94 @@ class _ShippingAddressScreenState extends State<ShippingAddressScreen> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
-          child: Column(
-            children: [
-              Text(
-                "Where will your order\nbe delivered?",
-                style: TextStyle(
-                  fontSize: 18,
-                  letterSpacing: 1.7,
-                  fontWeight: FontWeight.w600,
-
-                  color: Colors.black87,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Text(
+                  "Where will your order\nbe delivered?",
+                  style: TextStyle(
+                    fontSize: 18,
+                    letterSpacing: 1.7,
+                    fontWeight: FontWeight.w600,
+            
+                    color: Colors.black87,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "State",
-                  labelStyle: TextStyle(color: Colors.black54),
-                  border: OutlineInputBorder(),
+                SizedBox(height: 20),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter state';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "State",
+                    labelStyle: TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "City",
-                  labelStyle: TextStyle(color: Colors.black54),
-                  border: OutlineInputBorder(),
+                SizedBox(height: 10),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter city';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "City",
+                    labelStyle: TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Locality",
-                  labelStyle: TextStyle(color: Colors.black54),
-                  border: OutlineInputBorder(),
+                SizedBox(height: 10),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter locality';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Locality",
+                    labelStyle: TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 50,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 168, 137, 255),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: Text(
-              "Save",
-              style: GoogleFonts.montserrat(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
+        child: InkWell(
+          onTap: () {
+            if (_formKey.currentState!.validate()) {
+              print('validation successful');
+            }
+            else {
+              print('validation failed');
+            }
+          },
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 50,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 168, 137, 255),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(
+                "Save",
+                style: GoogleFonts.montserrat(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
