125711963571156a35394032b63a69500cd47dd7
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-08-31 23:20:30 +0530

    custom appbar

--- lib/views/screens/detail/screens/order_screen.dart
+++ lib/views/screens/detail/screens/order_screen.dart
@@ -41,6 +41,70 @@ class _OrderScreenState extends ConsumerState<OrderScreen> {
   Widget build(BuildContext context) {
     final orders = ref.watch(orderProvider);
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).size.height * 0.2,
+        ),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 118,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/icons/cartb.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 322,
+                top: 52,
+                child: Stack(
+                  children: [
+                    Image.asset("assets/icons/not.png", width: 25, height: 25),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 250, 201),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            orders.length.toString(),
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 61,
+                top: 51,
+                child: Text(
+                  'My Orders',
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body:
           orders.isEmpty
               ? const Center(child: Text('No orders found'))
