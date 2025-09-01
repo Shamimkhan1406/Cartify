65fc26957da367f3787006467f2dd2cd05883aa0
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-01 20:31:53 +0530

    displaying products with state management

--- lib/views/screens/nav_screens/widgets/popular_products_widget.dart
+++ lib/views/screens/nav_screens/widgets/popular_products_widget.dart
@@ -1,38 +1,57 @@
 import 'package:cartify/controllers/product_controller.dart';
-import 'package:cartify/models/product.dart';
+import 'package:cartify/provider/product_provider.dart';
 import 'package:cartify/views/screens/nav_screens/widgets/product_item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PopularProductsWidget extends StatefulWidget {
+class PopularProductsWidget extends ConsumerStatefulWidget {
   const PopularProductsWidget({super.key});
 
   @override
-  State<PopularProductsWidget> createState() => _PopularProductsWidgetState();
+  ConsumerState<PopularProductsWidget> createState() => _PopularProductsWidgetState();
 }
 
-class _PopularProductsWidgetState extends State<PopularProductsWidget> {
+class _PopularProductsWidgetState extends ConsumerState<PopularProductsWidget> {
   // a Future that will hold the list of popular products
-  late Future<List<Product>> futurePopularProductsFuture;
+  //late Future<List<Product>> futurePopularProductsFuture;
   @override
   void initState() {
     super.initState();
-    futurePopularProductsFuture = ProductController().loadPopularProducts();
+    //futurePopularProductsFuture = ProductController().loadPopularProducts();
+    _fetchProducts();
   }
+  Future<void> _fetchProducts() async{
+      final ProductController productController = ProductController();
+      try {
+        final products = await productController.loadPopularProducts();
+        ref.read(productProvider.notifier).setProducts(products);
+      } catch (e) {
+        print('Error fetching products: $e');
+      }
+    }
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(future: futurePopularProductsFuture, builder: (context, snapshot){
-      if (snapshot.connectionState==ConnectionState.waiting){
-        return const Center(child: CircularProgressIndicator());
-      }
-      else if (snapshot.hasError){
-        return Center(child: Text('Error: ${snapshot.error}'));
-      }
-      else if (!snapshot.hasData || snapshot.data!.isEmpty){
-        return const Center(child: Text('No popular products found'));
-      }
-      else{
-        final products = snapshot.data!;
-        return SizedBox(
+    // return FutureBuilder(future: futurePopularProductsFuture, builder: (context, snapshot){
+    //   if (snapshot.connectionState==ConnectionState.waiting){
+    //     return const Center(child: CircularProgressIndicator());
+    //   }
+    //   else if (snapshot.hasError){
+    //     return Center(child: Text('Error: ${snapshot.error}'));
+    //   }
+    //   else if (!snapshot.hasData || snapshot.data!.isEmpty){
+    //     return const Center(child: Text('No popular products found'));
+    //   }
+    //   else{
+    //     final products = snapshot.data!;
+    //     return 
+
+    //   }
+    // });
+    final products = ref.watch(productProvider);
+    if (products.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return SizedBox(
           height: 250,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
@@ -43,8 +62,5 @@ class _PopularProductsWidgetState extends State<PopularProductsWidget> {
             },
           ),
         );
-
-      }
-    });
   }
 }
