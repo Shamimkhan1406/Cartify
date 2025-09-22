25f0faf66900ad85991900604e6e9f6d8785335b
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-22 18:28:10 +0530

    Expose instance

---
+++ lib/provider/favorite_provider.dart
@@ -0,0 +1,43 @@
+import 'package:cartify/models/favorite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FavoriteNotifier extends StateNotifier<Map<String, Favorite>> {
+  FavoriteNotifier() : super({});
+
+  void addFavorite({
+    required String productName,
+    required double productPrice,
+    required String category,
+    required List<String> images,
+    required String vendorId,
+    required int productQuantity,
+    required int quantity,
+    required String productId,
+    required String description,
+    required String fullName,
+  }) {
+    state[productId] = Favorite(
+      productName: productName,
+      productPrice: productPrice,
+      category: category,
+      images: images,
+      vendorId: vendorId,
+      productQuantity: productQuantity,
+      quantity: quantity,
+      productId: productId,
+      description: description,
+      fullName: fullName,
+    );
+    // Notify listeners about the state change
+    state = {...state};
+  }
+  // method to remove a product from the favorite list
+  void removeFavoriteItem(String productId){
+    state.remove(productId);
+    // notify listeners about the state change
+    state = {...state};
+  }
+
+  // getter to retrieve the current Favorite items
+  Map<String, Favorite> get getCartItems => state;
+}
