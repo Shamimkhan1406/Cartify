6c7d725cea685dee9fc0ab2e84d689b8de85a469
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-22 18:13:19 +0530

    created wishlist model

---
+++ lib/models/favorite.dart
@@ -0,0 +1,15 @@
+class Favorite{
+  final String productName;
+  final double productPrice;
+  final String category;
+  final List<String> images;
+  final String vendorId;
+  final int productQuantity;
+  int quantity;
+  final String productId;
+  final String description;
+  final String fullName;
+
+  Favorite({required this.productName, required this.productPrice, required this.category, required this.images, required this.vendorId, required this.productQuantity, required this.quantity, required this.productId, required this.description, required this.fullName});
+
+}
