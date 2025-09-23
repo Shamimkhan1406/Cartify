6f8df316873fcd6626027987daa2d5c51373cb1e
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-23 21:38:28 +0530

    implement json support in cart model

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,4 +1,7 @@
-class Cart{
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Cart {
   final String productName;
   final double productPrice;
   final String category;
@@ -12,4 +15,38 @@ class Cart{
 
   Cart({required this.productName, required this.productPrice, required this.category, required this.images, required this.vendorId, required this.productQuantity, required this.quantity, required this.productId, required this.description, required this.fullName});
 
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'productName': productName,
+      'productPrice': productPrice,
+      'category': category,
+      'images': images,
+      'vendorId': vendorId,
+      'productQuantity': productQuantity,
+      'quantity': quantity,
+      'productId': productId,
+      'description': description,
+      'fullName': fullName,
+    };
+  }
+
+  factory Cart.fromMap(Map<String, dynamic> map) {
+    return Cart(
+      productName: map['productName'] as String,
+      productPrice: map['productPrice'] as double,
+      category: map['category'] as String,
+      images: List<String>.from((map['images'] as List<dynamic>)),
+      vendorId: map['vendorId'] as String,
+      productQuantity: map['productQuantity'] as int,
+      quantity: map['quantity'] as int,
+      productId: map['productId'] as String,
+      description: map['description'] as String,
+      fullName: map['fullName'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Cart.fromJson(String source) => Cart.fromMap(json.decode(source) as Map<String, dynamic>);
 }
