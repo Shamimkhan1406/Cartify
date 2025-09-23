d6f565a6ec50ec42990905a022ccc356219b7f17
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-23 21:16:11 +0530

    presist items in shared prefference

--- lib/provider/favorite_provider.dart
+++ lib/provider/favorite_provider.dart
@@ -1,9 +1,21 @@
+import 'dart:convert';
+
 import 'package:cartify/models/favorite.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoriteNotifier extends StateNotifier<Map<String, Favorite>> {
   FavoriteNotifier() : super({});
 
+  // a private method that saves the current list of favorite items to shared preferences
+  Future <void> _saveFavorites() async {
+    //retrive the shared preferences instance
+    final pref = await SharedPreferences.getInstance();
+    // convert the current state of favorite items to a list of json strings
+    final favoriteList = jsonEncode(state);
+    // save the list of json strings to shared preferences
+    await pref.setString('favorite', favoriteList);
+  }
   void addFavorite({
     required String productName,
     required double productPrice,
@@ -30,12 +42,14 @@ class FavoriteNotifier extends StateNotifier<Map<String, Favorite>> {
     );
     // Notify listeners about the state change
     state = {...state};
+    _saveFavorites();
   }
   // method to remove a product from the favorite list
   void removeFavoriteItem(String productId){
     state.remove(productId);
     // notify listeners about the state change
     state = {...state};
+    _saveFavorites();
   }
 
   // getter to retrieve the current Favorite items
