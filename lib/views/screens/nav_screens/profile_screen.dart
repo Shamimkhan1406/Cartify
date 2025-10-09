b035248a26026d0c67780747f87a69ad7569a558
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-10-09 20:48:46 +0530

    account screen ui building part 3

--- lib/views/screens/nav_screens/profile_screen.dart
+++ lib/views/screens/nav_screens/profile_screen.dart
@@ -153,6 +153,116 @@ class ProfileScreen extends StatelessWidget {
                               ),
                             ),
                           ),
+                          Positioned(
+                            left: 130,
+                            top: 66,
+                            child: Text(
+                              '5',
+                              style: GoogleFonts.roadRage(
+                                color: Colors.white,
+                                fontSize: 22,
+                                letterSpacing: 0.4,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 100,
+                            top: 99,
+                            child: Text(
+                              'Favorites',
+                              style: GoogleFonts.montserrat(
+                                fontSize: 14,
+                                color: Colors.white,
+                                letterSpacing: 0.3,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 114,
+                            top: 2,
+                            child: Container(
+                              width: 52,
+                              height: 58,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    "https://storage.googleapis.com/codeless-dev.appspot.com/uploads%2Fimages%2Fnn2Ldqjoc2Xp89Y7Wfzf%2Ff0db1e22e37c1e2a001bbb5bd4b9aafc.png",
+                                  ),
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                              child: Stack(
+                                clipBehavior: Clip.none,
+                                children: [
+                                  Positioned(
+                                    left: 13,
+                                    top: 18,
+                                    child: Image.network(
+                                      width: 26,
+                                      height: 26,
+                                      "https://firebasestorage.googleapis.com/v0/b/codeless-app.appspot.com/o/projects%2Fnn2Ldqjoc2Xp89Y7Wfzf%2F068bdad59a9aff5a9ee67737678b8d5438866afewish-list%201.png?alt=media&token=4a8abc27-022f-4a53-8f07-8c10791468e4",
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 20,
+                            top: 66,
+                            child: Text(
+                              '20',
+                              style: GoogleFonts.montserrat(
+                                color: Colors.white,
+                                fontSize: 22,
+                                letterSpacing: 0.4,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 13,
+                            top: 99,
+                            child: Text(
+                              'Cart',
+                              style: GoogleFonts.montserrat(
+                                fontSize: 14,
+                                color: Colors.white,
+                                letterSpacing: 0.4,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 56,
+                              height: 63,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    'https://storage.googleapis.com/codeless-dev.appspot.com/uploads%2Fimages%2Fnn2Ldqjoc2Xp89Y7Wfzf%2Fe0080f58f1ec1f2200fcf329b10ce4c4.png',
+                                  ),
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                              child: Stack(
+                                clipBehavior: Clip.none,
+                                children: [
+                                  Positioned(
+                                    left: 12,
+                                    top: 15,
+                                    child: Image.network(
+                                      width: 33,
+                                      height: 33,
+                                      'https://firebasestorage.googleapis.com/v0/b/codeless-app.appspot.com/o/projects%2Fnn2Ldqjoc2Xp89Y7Wfzf%2Fc2afb7fb33cd20f4f1aed312669aa43b8bb2d431cart%20(2)%201.png?alt=media&token=be3d8494-1ccd-4925-91f1-ee30402dfb0e',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
