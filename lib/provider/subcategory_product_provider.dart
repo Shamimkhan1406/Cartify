a416e6726a7c202acb11a6d0b2263e600914f522
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-10-12 20:14:51 +0530

    subcategory product state management

---
+++ lib/provider/subcategory_product_provider.dart
@@ -0,0 +1,12 @@
+import 'package:cartify/models/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SubcategoryProductProvider extends StateNotifier<List<Product>>{
+  SubcategoryProductProvider() : super([]);
+  // set the list of products
+  void setProducts(List<Product> products){
+    state = products;
+  }
+}
+
+final subcategoryProductProvider = StateNotifierProvider<SubcategoryProductProvider, List<Product>>((ref) => SubcategoryProductProvider(),);
