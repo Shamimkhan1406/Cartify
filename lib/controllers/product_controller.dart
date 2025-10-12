439ecdd7c20b1f813d82291e83d431b852aa0e5c
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-10-12 20:11:40 +0530

    consume subcategory product api

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -128,6 +128,37 @@ class ProductController {
     }
   }
 
+  // load product by subcategory
+  Future<List<Product>> loadProductsBySubCategory(String subCategory)async {
+    try {
+      http.Response response = await http.get(Uri.parse('$uri/api/producs-by-subcategory/$subCategory'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      // check the http response status code
+      if (response.statusCode == 200) {
+        // decode the json response body into list of dynamic objects type
+        final List<dynamic> data = jsonDecode(response.body) as List<dynamic>;
+
+        // map the list of dynamic objects to a list of Product objects
+        List<Product> relatedProducts = data.map((product)=> Product.fromMap(product as Map<String, dynamic>)).toList();
+        return relatedProducts;
+      } 
+      else if(response.statusCode == 404){
+        return [];
+      }
+      else {
+        // if the response status code is not 200, throw an error
+        throw Exception('Failed to load SubCategory Products');
+      }
+    } catch (e) {
+      // Handle error
+      throw Exception('Error loading SubCategory Products by subcategory: $e');
+      
+    }
+  }
+
   // method to fetch top 10 highest rated products
   Future<List<Product>> loadTopRatedProducts()async {
     try {
